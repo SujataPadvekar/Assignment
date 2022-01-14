f525898d0cba8570ff92b32506cf9dc70ea97cc4
SujataPadvekar <97655254+SujataPadvekar@users.noreply.github.com>
2022-01-14 22:49:57 +0530

    Add files via upload

---
+++ About.java
@@ -0,0 +1,15 @@
+class About
+{
+public static void main(String args[])
+{
+System.out.println("Name:- Sujata Padvekar \nPh.No.:-8625072497");
+}
+}
+
+Result:
+
+C:\Users\Hp\Desktop>javac About.java
+
+C:\Users\Hp\Desktop>java About
+Name:- Sujata Padvekar
+Ph.No.:-8625072497
